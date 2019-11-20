22c04ebd4c39ad96ceae2a4881d844ea6038ae01
Kosman <ok185022@ncr.com>
2019-11-20 11:53:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import Person from './Person/Person';
+//import Radium, {StyleRoot} from 'radium';
+import styled from 'styled-components';
+import './App.css';
+
+const StyledButton = styled.button`
+    background-color: ${props => props.altStyle ? 'red' : 'green'};
+    color: white;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 8px;
+    cursor: pointer;
+    &:hover {
+        background-color: ${props => props.altStyle ? 'salmon' : 'lightGreen'};
+        color: black;
+    }
+`;
+
+class App extends Component {
+    state = {
+        persons: [
+            { id: 1, name: 'Ori', age: 30 },
+            { id: 2, name: 'John Doe', age: 31 },
+            { id: 3, name: 'Jane Doe', age: 28 }
+        ],
+        otherState: 'Some other value',
+        showPersons: false
+    };
+
+    deletePersonHandler = personIndex => {
+        //const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({ persons: persons });
+    };
+
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        person.name = event.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({ persons: persons });
+    };
+
+    togglepersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({ showPersons: !doesShow });
+    };
+
+    render() {
+        const style = {
+            backgroundColor: 'lightGreen',
+            color: 'white',
+            font: 'inherit',
+            border: '1px solid #ccc',
+            padding: '8px',
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'green',
+                color: 'black'
+            }
+        };
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return (
+                            <Person
+                                click={() => this.deletePersonHandler(index)}
+                                name={person.name}
+                                age={person.age}
+                                key={person.id}
+                                changed={event =>
+                                    this.nameChangedHandler(event, person.id)
+                                }
+                            />
+                        );
+                    })}
+                </div>
+            );
+
+            // style.backgroundColor = 'Red';
+            // style[':hover'] = {
+            //     backgroundColor: 'salmon',
+            //     color: 'black'
+            // };
+        }
+
+        let classes = [];
+        if (this.state.persons.length <= 2) {
+            // classes = ['red']
+            classes.push('red');
+        }
+
+        if (this.state.persons.length <= 1) {
+            // classes = ['red', 'bold']
+            classes.push('bold');
+        }
+
+        return (
+            //<StyleRoot>
+            <div className='App'>
+                <h1>Almost Empty React App</h1>
+                <p className={classes.join(' ')}>
+                    This is a secondary Headline
+                </p>
+                <StyledButton altStyle={this.state.showPersons} onClick={this.togglepersonsHandler}>
+                    Toggle persons
+                </StyledButton>
+                {persons}
+            </div>
+            //</StyleRoot>
+        );
+    }
+}
+
+//export default Radium(App);
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+//import Radium from 'radium';
+import styled from 'styled-components';
+
+//import './Person.css';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+        width: 450px;
+    }
+`;
+
+const person = props => {
+    //To be used (via Radium) in the root component with 'StyleRoot' for media queries
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+
+    return (
+        //<div className='Person' style={style}>
+        <StyledDiv>
+            <p onClick={props.click}>
+                Hi! my name is {props.name}. I'm {props.age} years old
+            </p>
+            <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.name} />
+        </StyledDiv>
+        //</div>
+    );
+};
+
+//export default Radium(person);
+export default person;
